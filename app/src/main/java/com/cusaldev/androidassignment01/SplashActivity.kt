3b5ad8dea453c98987821c9ed176f2b921a4773b
Kusal Damsara <kusal2002d@Gmail.com>
2025-03-26 22:38:38 +0530

    Designed the main activity and connect that activity to the splash screen button.

--- app/src/main/java/com/cusaldev/androidassignment01/SplashActivity.kt
+++ app/src/main/java/com/cusaldev/androidassignment01/SplashActivity.kt
@@ -1,12 +1,17 @@
 package com.cusaldev.androidassignment01
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SplashActivity : AppCompatActivity() {
+
+    lateinit var btn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,14 @@ class SplashActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        btn = findViewById(R.id.button)
+
+        btn.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
