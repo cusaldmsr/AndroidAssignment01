572ee6aa48349796874fb8558c7c6a2c903f3f3a
Kusal Damsara <kusal2002d@Gmail.com>
2025-03-27 00:22:31 +0530

    Create EndActivity and update design

--- app/src/main/java/com/cusaldev/androidassignment01/EndActivity.kt
+++ app/src/main/java/com/cusaldev/androidassignment01/EndActivity.kt
@@ -1,32 +1,20 @@
 package com.cusaldev.androidassignment01
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-    lateinit var btn: Button
-
+class EndActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_end)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        btn = findViewById(R.id.button2)
-
-        btn.setOnClickListener {
-            val intent = Intent(this,HomeActivity::class.java)
-            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/cusaldev/androidassignment01/HomeActivity.kt
+++ app/src/main/java/com/cusaldev/androidassignment01/HomeActivity.kt
@@ -1,12 +1,18 @@
 package com.cusaldev.androidassignment01
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var btn1: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,13 @@ class HomeActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        btn1 = findViewById(R.id.imageButton6)
+
+        btn1.setOnClickListener {
+            val intent = Intent(this,EndActivity::class.java)
+            startActivity(intent)
         }
     }
 }
