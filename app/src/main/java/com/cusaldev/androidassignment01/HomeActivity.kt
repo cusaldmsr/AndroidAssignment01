a1f11cdcf540f7357fe6e8adbb623341a1e22a10
Kusal Damsara <kusal2002d@Gmail.com>
2025-03-27 12:22:51 +0530

    Validate radio buttons on HomeActivity

--- app/src/main/java/com/cusaldev/androidassignment01/HomeActivity.kt
+++ app/src/main/java/com/cusaldev/androidassignment01/HomeActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.RadioButton
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,13 +20,28 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
 
         btn1 = findViewById(R.id.imageButton6)
+        val r1 = findViewById<RadioButton>(R.id.radioButton)
+        val r2 = findViewById<RadioButton>(R.id.radioButton2)
+
+        r1.setOnCheckedChangeListener { buttonView, isChecked ->
+            Toast.makeText(this,r1.text, Toast.LENGTH_SHORT).show()
+        }
+
+        r2.setOnCheckedChangeListener { buttonView, isChecked ->
+            Toast.makeText(this,r2.text, Toast.LENGTH_SHORT).show()
+        }
 
         btn1.setOnClickListener {
             val intent = Intent(this,EndActivity::class.java)
