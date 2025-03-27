1675b8b152727de0a147136097586d0d7d465ef8
Kusal Damsara <kusal2002d@Gmail.com>
2025-03-27 12:25:08 +0530

    Removed unused import statements

--- app/src/main/java/com/cusaldev/androidassignment01/HomeActivity.kt
+++ app/src/main/java/com/cusaldev/androidassignment01/HomeActivity.kt
@@ -2,10 +2,8 @@ package com.cusaldev.androidassignment01
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.RadioButton
-import android.widget.Spinner
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
